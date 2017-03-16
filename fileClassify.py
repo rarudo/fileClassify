b9c05ad1878b0dd35c724578490f4e6060366936
rarudo <rarudonet@gmail.com>
2017-03-16 18:04:08 +0900

    Fix for work in current dirctory

--- fileClassify.py
+++ fileClassify.py
@@ -6,7 +6,7 @@ import io, sys
 # スクリプトを実行するフォルダを指定
 # デフォルトはスクリプトが置かれたフォルダ
 workDir = os.getcwd() + "/"
-workDir = "/Users/user/Downloads/tesPython/tesDir/"
+#workDir = "/Users/user/Downloads/tesPython/tesDir2/"
 
 # スペースで分割できなかった場合、指定された文字で分割
 splitList = ["-",
